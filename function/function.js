de6b3241a4f8cef638395d8c90710e00b8b808b3
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-02 21:50:10 +0600

    func

--- function/function.js
+++ function/function.js
@@ -1,9 +1,19 @@
-function myFirstFunc(number1 , number2) {
-    const result = number1 * number2;
-    console.log(result);
+// function myFirstFunc(number1 , number2) {
+//     const result = number1 * number2;
+//     console.log(result);
+    
+// }
+// myFirstFunc(4 , 5);
+// const any1 = 2;
+// const any2 = 3;
+// myFirstFunc(any1,any2)
+
+
+function myFunc2 (num1) {
+    const res = num1 * 11;
+    return res;
     
 }
-myFirstFunc(4 , 5);
-const any1 = 2;
-const any2 = 3;
-myFirstFunc(any1,any2)
+myFunc2(2)
+const store = myFunc2(3);
+console.log(store);
