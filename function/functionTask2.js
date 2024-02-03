ca16b2543f72ab389b0b0747d6d3f5a252745368
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-03 09:55:52 +0600

    task2

---
+++ function/functionTask2.js
@@ -0,0 +1,15 @@
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+// ans:
+
+function myNum(params) {
+  let result = 0;
+  if (params % 2 === 0) {
+    result = params / 2;
+    // console.log(result);
+  }
+  return result;
+}
+const num = 12;
+let n = myNum(num);
+console.log(n);
