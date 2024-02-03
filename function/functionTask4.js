ee806b34274a375306efabf602428451fe54316f
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-03 15:22:26 +0600

    task

--- function/functionTask4.js
+++ function/functionTask4.js
@@ -1,7 +1,7 @@
 // Task-4
 // Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
 // ans:
-function name(params) {
+function count_zero(params) {
     sum = [];
     for (let i = 0; i < params.length; i++) {
         if (params[i] == 0) {
@@ -11,6 +11,6 @@ function name(params) {
     return sum;
 }
 
-const res ="170000000000007890";
-const lets =name(res);
+const res ="11100001110010010000010";
+const lets =count_zero(res);
 console.log(lets);
