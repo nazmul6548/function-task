7f2f2243881a0430c2311cb28d3b967a874a08f0
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-04 23:52:46 +0600

    task

--- object task/object2.js
+++ object task/object2.js
@@ -1,9 +1,10 @@
-const myNum = [99, 100, 101, 102, 103, 104, 105, 1090, 3];
+// largest number
+const myNum = [99, 100, 101, 102, 103, 104, 105, 1090,0, 3];
 function myFunc(params) {
   // console.log(params);
-  let myStore = 0;
+  let myStore =0;
   for (let i = 0; i < params.length; i++) {
-    if (params[i] > myStore) {
+    if (myStore < params[i]) {
       myStore = params[i];
     }
   }
@@ -11,3 +12,20 @@ function myFunc(params) {
 }
 const result = myFunc(myNum);
 console.log(result);
+
+
+
+// smaller number
+const myNum1 = [66,67,68,69,70,71,72,73,74,75];
+function myFunc1(params1) {
+  // console.log(params);
+  let myStore =params1[0];
+  for (let i = 0; i < params1.length; i++) {
+    if (myStore > params1[i]) {
+      myStore = params1[i];
+    }
+  }
+  return myStore;
+}
+const result1 = myFunc1(myNum1);
+console.log(result1);
