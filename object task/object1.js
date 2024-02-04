0cc2c686aa95e7b07583b06908d193c9f759ce22
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-04 19:18:03 +0600

    object

---
+++ object task/object1.js
@@ -0,0 +1,24 @@
+const myArray = [4, 5, 6, 7, 8]
+
+function addToArray(params) {
+    console.log(`my array number : ${params[2]}`);
+}
+addToArray(myArray);
+
+
+
+
+
+
+
+const myArray1 = [15,44,444, 6];
+function name(params) {
+    console.log(`my number is : ${params[2]}`);
+}
+name(myArray1);
+
+
+function name(params) {
+    console.log(`my name is ${params}`);
+}
+name("jewel")
