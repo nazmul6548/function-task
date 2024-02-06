950dd9d64e8b1e2037bd83b1c2824c2c6e168f4f
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-06 15:51:49 +0600

    task4

---
+++ batch8ProblemSolving/problem4.js
@@ -0,0 +1,14 @@
+function findAdress(params) {
+  const value1 = params.street || "__";
+  const value2 = params.house || "__";
+  const value3 = params.society || "__";
+  const myAdress = value1 + "," + value2 + "," + value3;
+  return myAdress;
+}
+const myObject = {
+  street: 10,
+  house: "15A",
+  society: "Earth perfect",
+};
+const objectOutput = findAdress(myObject);
+console.log(objectOutput);
