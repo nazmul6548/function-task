775463cb2dbdd1f82628e4c24eeb9f5231038fa9
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-07 14:03:22 +0600

    modify

--- function/functionTask3.js
+++ function/functionTask3.js
@@ -15,3 +15,30 @@ function make_avg(params) {
 const myNum = [32 , 25 , 23 , 24 , 26 ];
 const result = make_avg(myNum);
 console.log(result);
+
+
+
+
+
+
+
+
+
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function myFunction(params) {
+  let sums = 0;
+  for (let i = 0; i < params.length; i++) {
+    sums = sums + params[i];
+    
+    
+  }
+  let avarage = sums / params.length;
+  // return  parseInt(avarage);
+  // let ava = Math.round(avarage);
+  let avar = avarage.toFixed(2)
+  return avar;
+  
+}
+const myArrayNumber = [3,3,4,6,7,8,9]
+console.log(myFunction(myArrayNumber));
